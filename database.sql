bb89d620c933f98a6bf1d0d136f37556ebf8d013
MD Tonmoy Islam Ruhin <110976855+TonmoyIslamRuhin1255@users.noreply.github.com>
2024-01-14 17:25:40 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2024 at 09:54 AM
+-- Generation Time: Jan 14, 2024 at 11:47 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `marer`
+-- Database: `project`
 --
 
 -- --------------------------------------------------------
@@ -28,11 +28,41 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `d` int(11) NOT NULL,
-  `dsdfds` int(11) NOT NULL,
-  `dfdsf` int(11) NOT NULL,
-  `dsfsdf` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `fullname` varchar(60) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullname`, `username`, `email`, `password`) VALUES
+(1, 'MD Ruhin Ahmed', 'md ruhinahmed183053', 'ruhinahmed93@gmail.com', ' $2y$10$W5Kw0anLrGy0Qz6r5nd4E.ksCpaDsQWICGpD61BlJWyyD3Ri5Z3ny'),
+(2, 'test test', 'testtest835349', 'test@gmail.com', ' $2y$10$UVMjXyT0YLZASlVqu7kmzeMQn7BZCRAreOgmZ5mCuRN.BAyl8pJHW'),
+(3, 'ab c', 'abc303695', 'abc@gmail.com', ' $2y$10$lOungkraAM4dYJ8.TX3aYO/utqBr9QwAvZcY6tP.7nHpNizg4NfA.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
